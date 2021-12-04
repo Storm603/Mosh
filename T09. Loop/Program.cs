472427c57b150ff7039add721e0d50dc8273cca5
Storm603 <93555141+Storm603@users.noreply.github.com>
2021-12-04 22:26:49 +0200

    Add files via upload

---
+++ T09. Loop/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace T09._Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] input = Console.ReadLine().Split(", ").ToArray();
+
+            while (input.Length < 5)
+            {
+                Console.WriteLine("Invalid List");
+
+                input = Console.ReadLine().Split(", ").ToArray();
+            }
+            Array.Sort(input);
+
+            for (int i = 0; i < 3; i++)
+            {
+                Console.WriteLine(input[i]);
+            }
+        }
+    }
+}
